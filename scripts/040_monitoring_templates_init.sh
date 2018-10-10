6651c44bd5c3cabf0e79699ff9f2dd8de14df4b6
Patrick Zambelli <patrick.zambelli@wuerth-phoenix.com>
2018-10-10 15:14:47 +0200

    update path for monitoring-templates in neteyeshare

--- scripts/040_monitoring_templates_init.sh
+++ scripts/040_monitoring_templates_init.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 #Define Variables
-FILESHARE_TEMPLATES="/neteye/shared/neteyeshare/monitoring"
-FILESHARE_MONIT_TEMPLATES="${FILESHARE_TEMPLATES}/icinga2-monitoring-templates/"
+FILESHARE_TEMPLATES="/neteye/shared/neteyeshare/monitoring/"
+FILESHARE_MONIT_TEMPLATES="monitoring-templates/"
 
 
 GIT_MONITORING_TEMPLATES="https://github.com/zampat/icinga2-monitoring-templates.git"
 
 # Clone Git Repo
-if [ ! -d ${FILESHARE_MONIT_TEMPLATES} ]
+if [ ! -d ${FILESHARE_TEMPLATES}${FILESHARE_MONIT_TEMPLATES} ]
 then
    LOC=`pwd`
    echo "[i] Fetching Monitoring Templates for import from git."
    mkdir -p $FILESHARE_TEMPLATES
 
    cd $FILESHARE_TEMPLATES
-   /usr/bin/git clone $GIT_MONITORING_TEMPLATES
+   /usr/bin/git clone $GIT_MONITORING_TEMPLATES ${FILESHARE_MONIT_TEMPLATES}
    echo "[i] Monitoring templates have been downloaded and are ready for import."
    echo "    For more info see $GIT_MONITORING_TEMPLATES"
 
