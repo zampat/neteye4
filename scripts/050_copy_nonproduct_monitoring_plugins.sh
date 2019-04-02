f9e8459398cccc9c34f029d3d8485fa0dc378dfd
Patrick Zambelli <patrick.zambelli@wuerth-phoenix.com>
2019-04-02 18:04:37 +0200

    synch monitoring plugins to neteyeshare

--- scripts/050_copy_nonproduct_monitoring_plugins.sh
+++ scripts/050_copy_nonproduct_monitoring_plugins.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 # Script action:
-# Copy all Monitoring Plugins from GIT folder "monitoring-plugins" to local neteyeshare folder.
+# Synch all Monitoring Plugins from GIT folder "monitoring-plugins" to local neteyeshare folder.
 #
 
 # PluginsContribDir: i.e. /neteye/shared/neteyeshare/monitoring
 NETEYESHARE_MONITORING=$1
 
-#Copy files ./monitoring/monitoring-plugins
-if [ ! -d ${NETEYESHARE_MONITORING}/monitoring-plugins/ ]
+#Define Variables
+SRC_GIT_MONIT_PLUGINS_FOLDER="./monitoring/monitoring-plugins"
+DST_MONIT_PLUGINS_FOLDER="${NETEYESHARE_MONITORING}/"
+
+# Verify DST Folder exists
+if [ -d "${DST_MONIT_PLUGINS_FOLDER}" ]
 then
-   echo "[ ] Copy content of configurations"
-   cp -r ./monitoring/monitoring-plugins $NETEYESHARE_MONITORING
+   echo "[i] Installing monitoring plugins (to ${DST_MONIT_PLUGINS_FOLDER})"
+   /usr/bin/rsync -av ${SRC_GIT_MONIT_PLUGINS_FOLDER} ${DST_MONIT_PLUGINS_FOLDER}/
+   echo "    Done: Monitoring Plugins have been installed."
+
+else
+   echo "[-] Abort installing additional monitoring plugins. Folder does not exist: ${DST_MONIT_PLUGINS_FOLDER}"
 fi
+
