ba39d2929c2a41f2860b526567cf4a3c37004b77
romboli67 <48832910+romboli67@users.noreply.github.com>
2019-04-03 14:14:55 +0200

    Update 020_get_icinga2_agents.sh

--- scripts/020_get_icinga2_agents.sh
+++ scripts/020_get_icinga2_agents.sh
@@ -3,6 +3,7 @@
 FOLDER_MONITORING_AGENT_MICROSOFT="$1/agents/microsoft/icinga"
 ICINGA2_AGENT_VERSION=$2
 ICINGA2_AGENT_PS_SCRIPT="/Icinga2Agent.psm1"
+SRC_GIT_AGENT_SCRIPTS_FOLDER="./monitoring/agents/microsoft/icinga"
 
 
 if [ ! -f "${FOLDER_MONITORING_AGENT_MICROSOFT}/Icinga2-v$ICINGA2_AGENT_VERSION-x86.msi" ]
@@ -26,3 +27,14 @@ else
    echo "    To update rename ${ICINGA2_AGENT_PS_SCRIPT} to ${ICINGA2_AGENT_PS_SCRIPT}.bak"
 fi
 
+
+# Install the Icinga2 Agent deployment scripts
+if [ -d "${FOLDER_MONITORING_AGENT_MICROSOFT}" ]
+then
+   echo "[i] Synchronizing Icinga2 Agent deployment scripts to neteyeshare (to ${FOLDER_MONITORING_AGENT_MICROSOFT})"
+   /usr/bin/rsync -av ${SRC_GIT_AGENT_SCRIPTS_FOLDER}/* ${FOLDER_MONITORING_AGENT_MICROSOFT}/
+   #echo "    Done: Host icons have been installed."
+
+else
+   echo "[-] Failed synchronization of Icinga2 agent deployment scripts. Folder: ${FOLDER_MONITORING_AGENT_MICROSOFT} does not exist."
+fi
