1d7145284c26044ae5dcf093b8de656c32142c5d
Patrick Zambelli <patrick.zambelli@wuerth-phoenix.com>
2019-04-11 17:28:39 +0200

    Provide Icinga2 Agent module as submodule in repo

--- scripts/020_get_icinga2_agents.sh
+++ scripts/020_get_icinga2_agents.sh
@@ -16,24 +16,12 @@ else
    echo "[ ] Icinga2 agent already installed"
 fi
 
-if [ ! -f "${FOLDER_MONITORING_AGENT_MICROSOFT}${ICINGA2_AGENT_PS_SCRIPT}" ]
-then
-   echo "[ ] Download latest version of Icinga2 Agent installation PS1 script for Windows"
-   echo "[!] Please rename ${ICINGA2_AGENT_PS_SCRIPT} to kickstart_icinga2_agent.ps1"
-   echo "[!] Append Director self service API key and parameters at the end."
-   wget https://raw.githubusercontent.com/Icinga/icinga2-powershell-module/master/Icinga2Agent/Icinga2Agent.psm1 -O ${FOLDER_MONITORING_AGENT_MICROSOFT}/${ICINGA2_AGENT_PS_SCRIPT}
-else
-   echo "[i] The ${ICINGA2_AGENT_PS_SCRIPT} already present in ${FOLDER_MONITORING_AGENT_MICROSOFT}"
-   echo "    To update rename ${ICINGA2_AGENT_PS_SCRIPT} to ${ICINGA2_AGENT_PS_SCRIPT}.bak"
-fi
 
-
-# Install the Icinga2 Agent deployment scripts
+# Sync the Icinga2 Agent deployment scripts
 if [ -d "${FOLDER_MONITORING_AGENT_MICROSOFT}" ]
 then
    echo "[i] Synchronizing Icinga2 Agent deployment scripts to neteyeshare (to ${FOLDER_MONITORING_AGENT_MICROSOFT})"
-   /usr/bin/rsync -av ${SRC_GIT_AGENT_SCRIPTS_FOLDER}/* ${FOLDER_MONITORING_AGENT_MICROSOFT}/
-   #echo "    Done: Host icons have been installed."
+   /usr/bin/rsync -av ${SRC_GIT_AGENT_SCRIPTS_FOLDER}/icinga2-powershell-module/Icinga2Agent/Icinga2Agent.psm1 ${FOLDER_MONITORING_AGENT_MICROSOFT}/Icinga2Agent.psm1.default
 
 else
    echo "[-] Failed synchronization of Icinga2 agent deployment scripts. Folder: ${FOLDER_MONITORING_AGENT_MICROSOFT} does not exist."
