3330786d6c713a5f77a6ab53a9a9096957199808
Patrick Zambelli <patrick.zambelli@wuerth-phoenix.com>
2019-05-08 11:55:34 +0200

    add authentication role examples for monitoring user

--- scripts/032_role_init.sh
+++ scripts/032_role_init.sh
@@ -11,9 +11,17 @@ if [ $RES -ne 0 ]
 then
    echo "[i] Adding Roles to authentication management."
    cat >>$FILE_RESSOURCES <<EOM
-[viewer]
+[monitoring_ro]
+permissions = "module/analytics, module/monitoring, module/neteye"
+
+[monitoring_operator]
 permissions = "module/monitoring, monitoring/command/*, monitoring/command/schedule-check, monitoring/command/acknowledge-problem, monitoring/command/remove-acknowledgement, monitoring/command/comment/*, monitoring/command/comment/add, monitoring/command/downtime/*, monitoring/command/downtime/schedule, monitoring/command/process-check-result"
-director/filter/hostgroups = "Hostgroup A, Hostgroup B"
+monitoring/filter/objects = "hostgroup_name=neteye servers"
+
+[monitoring_admin]
+permissions = "config/*, module/analytics, module/director, director/deploy, director/hosts, module/doc, module/monitoring, monitoring/command/*, director/showconfig"
+director/filter/hostgroups = "neteye servers"
+monitoring/filter/objects = "hostgroup_name=neteye servers"
 EOM
 
 else
