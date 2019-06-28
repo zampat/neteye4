5bbfd00d10a219ca3f3c158cce871defd522ddb3
Patrick Zambelli <patrick.zambelli@wuerth-phoenix.com>
2019-06-28 11:11:22 +0200

    synchronize new analytic dashboards to neteyeshare via rsync

--- scripts/062_sync_monitoring_analytics.sh
+++ scripts/062_sync_monitoring_analytics.sh
@@ -7,8 +7,12 @@
 NETEYESHARE_MONITORING=$1
 
 #Copy files ./monitoring/analytics_dashboards/
-if [ ! -d ${NETEYESHARE_MONITORING}/analytics_dashboards/ ]
+# Verify DST Folder exists
+if [ -d "${NETEYESHARE_MONITORING}" ]
 then
    echo "[+] 062: Copy analytics dashboards into $NETEYESHARE_MONITORING"
-   cp -r ./monitoring/analytics_dashboards $NETEYESHARE_MONITORING
+   /usr/bin/rsync -av ./monitoring/analytics_dashboards ${NETEYESHARE_MONITORING}/
+
+else
+   echo "[-] 062: Abort installing analytic dashboards to ${NETEYESHARE_MONITORING}. Folder does not exist."
 fi
