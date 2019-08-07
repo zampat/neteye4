0a944049c1be120b8dc8103c9eeaa3d90da71a8c
Patrick Zambelli <patrick.zambelli@wuerth-phoenix.com>
2019-08-07 11:11:06 +0200

    update default permission roles

--- scripts/032_role_init.sh
+++ scripts/032_role_init.sh
@@ -5,23 +5,26 @@
 FILE_RESSOURCES="$1/roles.ini"
 
 # Check if additional default roles already exists
-grep "viewer" $FILE_RESSOURCES > /dev/null 2>&1
+grep "monitoring_unlimited_operator" $FILE_RESSOURCES > /dev/null 2>&1
 RES=$?
 if [ $RES -ne 0 ]
 then
    echo "[i] 032: Adding Roles to authentication management."
    cat >>$FILE_RESSOURCES <<EOM
-[monitoring_ro]
-permissions = "module/analytics, module/monitoring, module/neteye"
+[monitoring_unlimited_operator]
+permissions = "application/share/navigation, application/log, config/*, module/analytics, module/auditlog, module/businessprocess, businessprocess/showall, businessprocess/create, businessprocess/modify, module/director, director/api, director/audit, director/showconfig, director/showsql, director/deploy, director/hosts, director/services, director/servicesets, director/service_set/apply, director/users, director/notifications, director/inspect, director/monitoring/services-ro, director/*, module/doc, module/fileshipper, module/geomap, geomap/admin, geomap/editor, geomap/viewer, module/idoreports, module/ipl, module/lampo, module/leafletjs, module/licenses, module/monitoring, monitoring/command/*, module/nagvis, nagvis/edit, nagvis/admin, nagvis/overview, module/neteye, module/pdfexport, module/reactbundle, module/reporting, module/tornado, module/vsphere, module/vspheredb"
 
-[monitoring_operator]
-permissions = "module/monitoring, monitoring/command/*, monitoring/command/schedule-check, monitoring/command/acknowledge-problem, monitoring/command/remove-acknowledgement, monitoring/command/comment/*, monitoring/command/comment/add, monitoring/command/downtime/*, monitoring/command/downtime/schedule, monitoring/command/process-check-result"
-monitoring/filter/objects = "hostgroup_name=neteye servers"
+[monitoring_unlimited_ro]
+permissions = "application/share/navigation, application/log, module/analytics, module/auditlog, module/doc, module/geomap, geomap/viewer, module/idoreports, module/ipl, module/lampo, module/leafletjs, module/monitoring, module/nagvis, nagvis/overview, module/neteye, module/pdfexport, module/reactbundle, module/reporting, module/tornado, module/vspheredb"
 
-[monitoring_admin]
+[windows_sys_operator]
+permissions = "module/analytics, module/doc, module/monitoring, monitoring/command/schedule-check, monitoring/command/acknowledge-problem, monitoring/command/remove-acknowledgement, monitoring/command/comment/*, monitoring/command/comment/add, monitoring/command/comment/delete, monitoring/command/downtime/*, monitoring/command/downtime/schedule, monitoring/command/downtime/delete, monitoring/command/process-check-result, module/pdfexport"
+monitoring/filter/objects = "(hostgroup_name=windows servers||hostgroup_name=microsoft sql servers)"
+
+[windows_sys_administrator]
 permissions = "config/*, module/analytics, module/director, director/deploy, director/hosts, module/doc, module/monitoring, monitoring/command/*, director/showconfig"
-director/filter/hostgroups = "neteye servers"
-monitoring/filter/objects = "hostgroup_name=neteye servers"
+director/filter/hostgroups = "windows servers"
+monitoring/filter/objects = "(hostgroup_name=windows servers||hostgroup_name=microsoft sql servers)"
 EOM
 
 else
